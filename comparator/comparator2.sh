d4432356dbba2fd4a1fcffacbc87b52b37e69eac
ofdun <elik394@mail.ru>
2023-12-07 21:22:34 +0300

    including \n in a comparison

--- comparator/comparator2.sh
+++ comparator/comparator2.sh
@@ -26,8 +26,8 @@ if ! [[ -f "$file1" && -f "$file2" ]]; then
 fi
 
 # Get files text after first string:.*
-string_file1=$(grep -z "string:.*" "$file1")
-string_file2=$(grep -z "string:.*" "$file2")
+string_file1=$(cat -A "$file1" | grep -zo "string:.*" | tr -d "\0")
+string_file2=$(cat -A "$file2" | grep -zo "string:.*" | tr -d "\0")
 
 if [ "$string_file1" == "" -o "$string_file2" == "" ]; then
     exit 5
